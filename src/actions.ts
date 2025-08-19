2382bde0a8c9e3d93c58e83314fe9e85d408bf0c
Phillip Pietruschka <Phillip.Pietruschka@parliament.vic.gov.au>
2025-08-20 08:14:40 +1000

    fix: action ids

--- src/actions.ts
+++ src/actions.ts
@@ -1,13 +1,20 @@
+import { CompanionActionDefinition } from '@companion-module/base'
 import type { ModuleInstance } from './main.js'
 
+export enum ActionId {
+	UpdateFeed = 'update_feed',
+}
+
 export function UpdateActions(self: ModuleInstance): void {
-	self.setActionDefinitions({
-		sample_action: {
+	const actions: { [id in ActionId]: CompanionActionDefinition } = {
+		[ActionId.UpdateFeed]: {
 			name: 'Update Feed',
 			options: [],
 			callback: async (_event, _context) => {
 				await self.updateFeed()
 			},
 		},
-	})
+	}
+
+	self.setActionDefinitions(actions)
 }
